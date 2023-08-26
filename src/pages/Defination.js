906cdf572239a8a88c63c9bf1a1e5a117779d42f
bungongtoto <kingsley.bungong@ictuniversity.edu.cm>
2023-08-26 23:35:01 +0100

    introduction to useFetch hooks

--- src/pages/Defination.js
+++ src/pages/Defination.js
@@ -1,31 +1,34 @@
 import { useEffect, useState } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { Link, useFetcher, useParams } from "react-router-dom";
 
 import { v4 as uuidv4 } from 'uuid';
 import NotFound from "../components/NotFound";
 import DefinationSearch from "../components/DefinationSearch";
+import useFetch from "../hooks/UseFetch";
 
 export default function Defination(){
-    const [word, setWord] = useState();
-    const [notFound, setNotFound] = useState(false);
+    // const [word, setWord] = useState();
+    //const [notFound, setNotFound] = useState(false);
     let {search} = useParams();
     //const navigate = useNavigate();
 
-    useEffect(() => {
-        fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + search)
-        .then((response) => {
-            if(response.status === 404){
-                setNotFound(true);
-                return null;
-            }
-           return response.json();
-        })
-        .then((data) => {
-            setWord(data ? data[0].meanings: null);
-        });
-    }, []);
+    const [word, errorStatus ]= useFetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + search);
 
-    if(notFound === true){
+    // useEffect(() => {
+    //     fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + search)
+    //     .then((response) => {
+    //         if(response.status === 404){
+    //             setNotFound(true);
+    //             return null;
+    //         }
+    //        return response.json();
+    //     })
+    //     .then((data) => {
+    //         setWord(data ? data[0].meanings: null);
+    //     });
+    // }, []);
+
+    if(errorStatus === 404){
         return (
             <>
                 <NotFound />
@@ -34,13 +37,22 @@ export default function Defination(){
         );
     }
 
+    if(errorStatus){
+        return (
+            <>
+                <p>there is a problem with the server, try again later.</p>
+                <Link to='/dictionary'>Search another</Link>
+            </>
+        );
+    }
+
     return (
         <>
            
-            {word ? 
+            {word?.[0]?.meanings ? (
             <>
                 <h1>Here is a defination: {search}</h1>
-                {word.map((meaning) => {
+                {word[0].meanings.map((meaning) => {
                     return(
                         <p key={uuidv4()}>
                             {meaning.partOfSpeech + ': '}
@@ -50,7 +62,7 @@ export default function Defination(){
                 })}
                 <p>Search Again:</p>
                 <DefinationSearch /> 
-            </> : <p>Loading .....</p> }
+            </> ): <p>Loading .....</p> }
         </>
     );
 }
